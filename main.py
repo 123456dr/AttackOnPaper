048f6cafae2a7f69c7703c7111cda58bfed73678
RC <125175307+123456dr@users.noreply.github.com>
2025-06-27 15:39:36 +0800

    feat: add video transformation feature and improve media selection
    - Added the video transformation feature with default preview switching.
    - Fixed issues where users could not toggle between image and video mode after uploading a file.
    - Improved user feedback for invalid or repeated uploads.
    - Adjusted UI buttons to align with interactive areas, preventing misclicks.

--- main.py
+++ main.py
@@ -90,6 +90,39 @@ async def upload_file( file: UploadFile = File(...)):
 
 
 
+
+output_video_path=f"RCaop{uuid.uuid4()}.mp4"
+#output_video_path = "output.mp4"
+#影格擷取與合成影片
+def cap_to_video(save_path, target_fps=12):
+    cap = cv2.VideoCapture(save_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    #fourcc = cv2.VideoWriter_fourcc(*'mp4v') # four carator cope
+    fourcc = cv2.VideoWriter_fourcc(*'avc1' )       
+    writer = cv2.VideoWriter(output_video_path, fourcc, target_fps, (width, height))
+    frame_interval = int(fps // target_fps)
+    count = 0
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        if count % frame_interval == 0:
+            frame = cv2.cvtColor( cv2.Canny(cv2.cvtColor(np.clip(frame * 1.5, 0, 255).astype(np.uint8), cv2.COLOR_BGR2GRAY), 150, 200), cv2.COLOR_BAYER_BG2BGR)#cv2.COLOR_GRAY2BGR) #3通道
+            #frame = cv2.cvtColor(cv2.Canny(frame, 150, 200), cv2.COLOR_BAYER_BG2BGR) #3通道
+            frame = 255-frame
+            writer.write(frame)
+        count += 1
+    
+    cap.release()
+    writer.release()
+    #return output_video_path
+
+
+
 @app.post("/upload_video")
 async def upload_video(file: UploadFile = File(...)): #, background_task:BackgroundTasks = BackgroundTasks()):
     save_path =  f"RCaop{uuid.uuid4()}.mp4" #"video.mp4" #
@@ -98,13 +131,20 @@ async def upload_video(file: UploadFile = File(...)): #, background_task:Backgro
         shutil.copyfileobj(file.file, buffer)
     
     #...暫時先不寫處理邏輯
-    save_path = "defalt.mp4"
+    #save_path = "defalt.mp4"
+    cap_to_video(save_path)
+    save_path = output_video_path#cap_to_video(save_path)
 
     # background_tasks.add_task(remove_file, save_path)
 
     return FileResponse(save_path, media_type="video/mp4")
 # 加上filename可自訂下載檔案名
 
+
+'''
+
+
+'''
 
 
 '''
