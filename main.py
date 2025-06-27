e1248b71ffb5ea937e4caa422efa4e4d185afd51
RC <125175307+123456dr@users.noreply.github.com>
2025-06-28 00:00:40 +0800

    update file to render

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# Swagger UI（點擊操作介面）：http://127.0.0.1:8000/docs
+# Redoc（文件版樣式）：http://127.0.0.1:8000/redoc
+
+# pip freeze > requirements.txt
+# 啟動腳本 uvicorn main:app --host 0.0.0.0 --port 10000
+# uvicorn main:app --reload 執行fastapi
+# render Build Command: pip install -r requirements.txt 
+# start Command: uvicorn main:app --host 0.0.0.0 --port $PORT && Port: 10000
+
+
 from fastapi import FastAPI,File,UploadFile,Form
 from pydantic import BaseModel
 from fastapi.responses import FileResponse #回傳圖片顯示
@@ -124,3 +134,45 @@ async def upload_video(file: UploadFile = File(...)): #, background_task:Backgro
     # background_tasks.add_task(remove_file, save_path)
 
     return FileResponse(save_path, media_type="video/mp4")
+# 加上filename可自訂下載檔案名
+
+
+'''
+
+
+'''
+
+
+'''
+# 第一版
+[] 將影片切成逐張圖片儲存至新創建的資料夾
+[] 將逐張影格做增強對比
+[] 將處理過後的影格逐張合成影片
+[] 回傳影片
+
+# 第二版
+[] 透過人物骨架做人物處理
+[] 骨架處理後可透過膨脹法
+
+
+# 第二版進階
+[] 串接或訓練ai做兩張相連影格間人物之動作預判與產生線稿
+[] 完成影格處理後，已前後兩張為單位，接續比對與修正人物線條顯示
+
+'''
+
+
+'''
+class Message(BaseModel):
+    name: str
+    age: int
+'''
+'''
+@app.get("/hello")
+def say_hello():
+    return {"message": "Hello FastAPI! "}
+
+@app.get("/ping")
+def a():
+    return {"status":"ok"}
+'''
